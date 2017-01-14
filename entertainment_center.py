d1d265347bb0382679d56f30182db978fef5e25e
ArthurJordao <arthurbernardijordao@gmail.com>
2017-01-14 15:03:17 -0200

    Add some comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,6 +3,7 @@ import fresh_tomatoes
 
 
 def main():
+    # Create movies here
     interstellar = Movie("Interstellar", "A science fiction film about space travalling",
                          "https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Interstellar_film_poster.jpg/220px-Interstellar_film_poster.jpg", "https://www.youtube.com/watch?v=zSWdZVtXT7E")
     divergent = Movie("Divergent", "A filme about a future society",
@@ -10,7 +11,9 @@ def main():
     maze_runner = Movie("The Maze Runner", "A filme about a society that lives in a maze",
                         "https://upload.wikimedia.org/wikipedia/en/b/be/The_Maze_Runner_poster.jpg", "https://www.youtube.com/watch?v=AwwbhhjQ9Xk")
 
+    # add your movies object in this array
     movies = [interstellar, divergent, maze_runner]
+    # Open the navigator
     fresh_tomatoes.open_movies_page(movies)
 
 if __name__ == "__main__":
